28173571ab70a3d5531787bee77724c93ee36d56
Rayn <rayn.tariq@gmail.com>
2024-11-10 13:38:21 +0530

    Add and organize directories

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Chat from "./pages/chat";
+
+const App = () => {
+		return <Home />;
+}
+
+export default App;
---
+++ Frontend/src/pages/home.js
@@ -0,0 +1,18 @@
+const Home = () => {
+	return (
+		<div className="bg-cover bg-center h-screen bg-[url('../public/image.png')]">
+			<div className="font-bold text-3xl">iShowChat</div>
+			<p>
+				Nisi fugiat reprehenderit dolor reprehenderit qui ex amet
+				pariatur. Sit ut excepteur deserunt Lorem enim deserunt cillum
+				id sit officia ea ut. Voluptate officia in adipisicing laborum
+				nisi et tempor velit Lorem commodo Lorem veniam fugiat ex. Elit
+				nulla nostrud velit velit eu exercitation enim.
+			</p>
+			<button className="btn">Login</button>
+			<button className="btn">Register</button>
+		</div>
+	);
+};
+
+export default Home;
