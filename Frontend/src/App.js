c1c5b8329192924da4b6570cf6d047f792c92874
Rayn <rayn.tariq@gmail.com>
2024-11-23 15:06:35 +0530

    Merge frontend and backend directories

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { useState } from "react";
-import Home from "./pages/home";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Chat from "./pages/chat";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ChatPage from './Pages/ChatPage'
+import LoginPage from './Pages/LoginPage'
+import RegisterPage from './Pages/RegisterPage'
+import ProfilePage from './Pages/ProfilePage'
+import CreateGroupPage from './Pages/CreateGroupPage'
+import GroupDetailsPage from './Pages/GroupDetailsPage'
 
 const App = () => {
-	return <Home />;
-};
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/creategroup" element={<CreateGroupPage/>} />
+        <Route path="/groupDetails" element={<GroupDetailsPage/>} />
+      </Routes>
+    </Router>
+  )
+}
 
-export default App;
+export default App
---
+++ Frontend/src/Components/SocketClient.js
@@ -0,0 +1,94 @@
+const { io } = require("socket.io-client");
+
+async function connectGroup(username, groupName) {
+	const socket = io("http://localhost:4000", {
+		auth: {
+			username: username,
+		},
+	});
+
+	socket.on("connect", () => {
+		console.log(`${username} Successfully Connected`);
+		socket.emit("join", groupName);
+
+        socket.emit("load-messages", groupName);
+
+		console.log("Previous messages retrieved:");
+		socket.on("previous-messages", (messages) => {
+			console.log(messages);
+		});
+		
+		socket.on("new-message", async (message) => {
+			try {
+				console.log(
+					`New Message from ${message.sender}:\n${message.content}`
+				);
+			} catch (error) {
+				console.error("Error Displaying New Message:", error);
+			}
+		});
+
+		socket.on("message-error", (error) => {
+			console.error("Socket message error:", error);
+		});
+	});
+
+	socket.on("connect-error", (error) => {
+		console.error("Connection Error:", error);
+	});
+
+	socket.on("disconnect", (reason) => {
+		console.log("Disconnected:", reason);
+	});
+}
+
+const sendMessage = async (username, groupName, message) => {
+    const socket = io("http://localhost:4000", {
+		auth: {
+			username: username,
+		},
+	});
+
+	socket.on("connect", () => {
+    try {
+        socket.emit("send-message", {
+            groupName: groupName,
+            content: message,
+        });
+    
+        console.log("Message send test initiated");
+    } catch (error) {
+        console.error("Message send test failed:", error);
+    }
+})
+}
+
+const loadMessage = async () => {
+	const socket = io("http://localhost:4000", {
+		auth: {
+			username: username,
+		},
+	});
+
+	socket.on("connect", () => {
+		console.log(`${username} Successfully Connected`);
+		socket.emit("join", groupName);
+
+        socket.emit("load-messages", groupName);
+
+		console.log("Previous messages retrieved:");
+		socket.on("previous-messages", (messages) => {
+			console.log(messages);
+		});
+	})
+}
+
+const disconnectGroup = async () => {
+	
+}
+
+module.exports = {
+    connectGroup,
+    sendMessage,
+	loadMessage
+};
